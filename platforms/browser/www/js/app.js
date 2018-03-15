dd6d66804c7ea715d0f99dc451737572db4e3089
sacharypens <sacharypens@gmail.com>
2018-03-15 22:39:17 +0100

    alle schermen klaargemaakt

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -2,6 +2,12 @@ $(function(){
 	document.addEventListener("deviceready", onDeviceReady, false);
 
     $(".button-collapse").sideNav();
+
+    $('.side-nav a').click(function(){
+        $('.spa').hide();
+        $('#' + $(this).data('show')).show();
+        $('.button-collapse').sideNav('hide');
+    });
 });
 
 function onDeviceReady() {
